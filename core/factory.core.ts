13bf701ad230d73d7e56c198c693df6dfe34591f
Utee <utibeabasiekong2@gmail.com>
2024-10-23 03:59:35 +0100

    fix: async issue with the factory fixed

--- core/factory.core.ts
+++ core/factory.core.ts
@@ -6,28 +6,22 @@ import { expressMiddleware } from '@apollo/server/express4';
 import { RequestHandler } from 'express';
 import clc from 'cli-color';
 
-export function startServer(
+export async function startServer(
     httpServer: Server<typeof IncomingMessage, typeof ServerResponse>,
     schema: GraphQLSchema,
-): RequestHandler<any> {
-    let server: ApolloServer<BaseContext>;
-
-    const internalFunc = async () => {
-        server = new ApolloServer({
+): Promise<RequestHandler<any>> {
+    try {
+        const server = new ApolloServer({
             schema,
             plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
         });
 
         await server.start();
-    };
 
-    internalFunc()
-        .then((result) => {})
-        .catch((err) => {
-            console.log(`${clc.red('GRAPHQL REGISTRAR ERROR: ')} ${err}`);
-        });
+        const middleware = expressMiddleware(server);
 
-    const middleware = expressMiddleware(server);
-
-    return middleware;
+        return middleware;
+    } catch (err: any) {
+        console.log(`${clc.red('GRAPHQL REGISTRAR ERROR: ')} ${err}`);
+    }
 }
